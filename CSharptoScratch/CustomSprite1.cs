bcbb101b3c00cb9bc9e4db0ba1475df3fa884aca
David Stall <davidstall312@gmail.com>
2026-05-06 09:06:33 -0700

    added better partial classes for sprites

---
+++ CSharptoScratch/CustomSprite1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharptoScratch
+{
+    internal partial class Sprite: ISpriteClass, IGreenFlagClass
+    {
+        public void CustomMethod()
+        {
+
+        }
+        public void CustomBlock()
+        {
+            looks.Hide();
+            motion.Move(10);
+        }
+        public void GreenFlag()
+        {
+            CustomBlock();
+        }
+    }
+}
--- CSharptoScratch/Sprite.cs
+++ CSharptoScratch/Sprite.cs
@@ -6,12 +6,33 @@ using System.Threading.Tasks;
 
 namespace CSharptoScratch
 {
-    internal class Sprite
+    internal interface IGreenFlagClass
     {
-        public Motion motion = new Motion();
-        public Looks looks = new Looks();
-        public Sound sound = new Sound();
-        public Sensing sensing = new Sensing();
-        public Pen pen = new Pen();
+        public abstract void GreenFlag();
+    }
+    internal interface ISpriteClass
+    {
+        public Motion motion { get; }
+        public Looks looks { get; }
+        public Sound sound { get; }
+        public Sensing sensing { get; }
+        public Pen pen { get; }
+    }
+    internal partial class Sprite : ISpriteClass
+    {
+        public string name { get; set; }
+        public Motion motion { get; private set; }
+        public Looks looks { get; private set; }
+        public Sound sound { get; private set; }
+        public Sensing sensing { get; private set; }
+        public Pen pen { get; private set; }
+        public Sprite()
+        {
+            motion = new Motion();
+            looks = new Looks();
+            sound = new Sound();
+            sensing = new Sensing();
+            pen = new Pen();
+        }
     }
 }
