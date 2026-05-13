40cc0767e24905ed679f5ed423052df0b23b42f0
DaviRK <2007641@apps.nsd.org>
2026-05-13 09:09:23 -0700

    Added the ability to add sprites. Stage is automatically added to sprites list. Added the beginning of a code group box for editing code.

--- CSharptoScratch/Form1.Designer.cs
+++ CSharptoScratch/Form1.Designer.cs
@@ -28,12 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listBoxSprites = new ListBox();
+            buttonAddSprite = new Button();
+            textBoxName = new TextBox();
+            labelName = new Label();
+            richTextBoxCode = new RichTextBox();
+            groupBoxCode = new GroupBox();
+            groupBoxSprites = new GroupBox();
+            buttonSave = new Button();
+            buttonCancel = new Button();
+            groupBoxCode.SuspendLayout();
+            groupBoxSprites.SuspendLayout();
+            SuspendLayout();
+            // 
+            // listBoxSprites
+            // 
+            listBoxSprites.FormattingEnabled = true;
+            listBoxSprites.ItemHeight = 15;
+            listBoxSprites.Location = new Point(6, 22);
+            listBoxSprites.Name = "listBoxSprites";
+            listBoxSprites.Size = new Size(120, 109);
+            listBoxSprites.TabIndex = 0;
+            listBoxSprites.SelectedIndexChanged += listBoxSprites_SelectedIndexChanged;
+            // 
+            // buttonAddSprite
+            // 
+            buttonAddSprite.Location = new Point(132, 66);
+            buttonAddSprite.Name = "buttonAddSprite";
+            buttonAddSprite.Size = new Size(120, 65);
+            buttonAddSprite.TabIndex = 2;
+            buttonAddSprite.Text = "Add Sprite";
+            buttonAddSprite.UseVisualStyleBackColor = true;
+            buttonAddSprite.Click += buttonAddSprite_Click;
+            // 
+            // textBoxName
+            // 
+            textBoxName.Location = new Point(132, 37);
+            textBoxName.Name = "textBoxName";
+            textBoxName.Size = new Size(120, 23);
+            textBoxName.TabIndex = 3;
+            // 
+            // labelName
+            // 
+            labelName.AutoSize = true;
+            labelName.Location = new Point(132, 19);
+            labelName.Name = "labelName";
+            labelName.Size = new Size(72, 15);
+            labelName.TabIndex = 4;
+            labelName.Text = "Sprite Name";
+            // 
+            // richTextBoxCode
+            // 
+            richTextBoxCode.Location = new Point(6, 22);
+            richTextBoxCode.Name = "richTextBoxCode";
+            richTextBoxCode.Size = new Size(234, 120);
+            richTextBoxCode.TabIndex = 5;
+            richTextBoxCode.Text = "";
+            // 
+            // groupBoxCode
+            // 
+            groupBoxCode.Controls.Add(buttonCancel);
+            groupBoxCode.Controls.Add(buttonSave);
+            groupBoxCode.Controls.Add(richTextBoxCode);
+            groupBoxCode.Location = new Point(321, 12);
+            groupBoxCode.Name = "groupBoxCode";
+            groupBoxCode.Size = new Size(246, 182);
+            groupBoxCode.TabIndex = 6;
+            groupBoxCode.TabStop = false;
+            groupBoxCode.Text = "Code";
+            // 
+            // groupBoxSprites
+            // 
+            groupBoxSprites.Controls.Add(listBoxSprites);
+            groupBoxSprites.Controls.Add(labelName);
+            groupBoxSprites.Controls.Add(buttonAddSprite);
+            groupBoxSprites.Controls.Add(textBoxName);
+            groupBoxSprites.Location = new Point(12, 12);
+            groupBoxSprites.Name = "groupBoxSprites";
+            groupBoxSprites.Size = new Size(261, 142);
+            groupBoxSprites.TabIndex = 7;
+            groupBoxSprites.TabStop = false;
+            groupBoxSprites.Text = "Sprites";
+            // 
+            // buttonSave
+            // 
+            buttonSave.Location = new Point(6, 148);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(114, 23);
+            buttonSave.TabIndex = 6;
+            buttonSave.Text = "Save";
+            buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            buttonCancel.Location = new Point(126, 148);
+            buttonCancel.Name = "buttonCancel";
+            buttonCancel.Size = new Size(114, 23);
+            buttonCancel.TabIndex = 7;
+            buttonCancel.Text = "Cancel";
+            buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBoxSprites);
+            Controls.Add(groupBoxCode);
+            Name = "Form1";
+            Text = "Form1";
+            groupBoxCode.ResumeLayout(false);
+            groupBoxSprites.ResumeLayout(false);
+            groupBoxSprites.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ListBox listBoxSprites;
+        private Label labelSprites;
+        private Button buttonAddSprite;
+        private TextBox textBoxName;
+        private Label labelName;
+        private RichTextBox richTextBoxCode;
+        private GroupBox groupBoxCode;
+        private GroupBox groupBoxSprites;
+        private Button buttonCancel;
+        private Button buttonSave;
     }
 }
--- CSharptoScratch/Form1.cs
+++ CSharptoScratch/Form1.cs
@@ -2,9 +2,41 @@ namespace CSharptoScratch
 {
     public partial class Form1 : Form
     {
+        List<Sprite> sprites = new List<Sprite>();
         public Form1()
         {
             InitializeComponent();
+            Sprite stage = new Sprite();
+            stage.name = "stage";
+            sprites.Add(stage);
+            listBoxSprites.Items.Add("stage");
+        }
+
+        private void buttonAddSprite_Click(object sender, EventArgs e)
+        {
+            string text = textBoxName.Text;
+            bool isDuplicate = false;
+            if (!text.Equals(""))
+            {
+                for (int i = 0; i < sprites.Count; i++)
+                {
+                    if (text == sprites[i].name) isDuplicate = true;
+                }
+
+                if (!isDuplicate)
+                {
+                    Sprite sprite = new Sprite();
+                    sprite.name = text;
+                    sprites.Add(sprite);
+                    listBoxSprites.Items.Add(sprite.name);
+                    textBoxName.Clear();
+                }
+            }
+        }
+
+        private void listBoxSprites_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
